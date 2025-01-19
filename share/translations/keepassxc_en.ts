858f8150addd300a6ce12b7f568145022163b8b8
Jonathan White <support@dmapps.us>
2025-01-19 12:06:35 -0500

    Enable access key underline of menu bar items when focused
    * Fixes #7325
    
    Qt 5.x defaults to always show mnemonic underlines in menus. Qt 6.x fixes this, but we can't port the fix since it relies on private theme namespaces. Anyway, to get around this, simply check if the widget is a menubar and focused. If so, we draw the underlines. Only applies when the user actually presses ALT so this is desired behavior.
    
    NOTE: Underlines in menus will disappear once you focus on the menu, still working on this

--- share/translations/keepassxc_en.ts
+++ share/translations/keepassxc_en.ts
@@ -5632,10 +5632,6 @@ Are you sure you want to continue with this file?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>View</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Theme</source>
         <translation type="unfinished"></translation>
     </message>
@@ -6298,6 +6294,10 @@ Expect some bugs and minor issues, this version is meant for testing purposes.</
     </message>
     <message>
         <source>E&amp;xpire Entry…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
